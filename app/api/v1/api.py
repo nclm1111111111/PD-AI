f3ca95dddf30b12c09039fb3b4a7ae1dc3596acc
66qjc <qjc661764@163.com>
2026-03-13 23:08:22 +0800

    ok

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from app.api.v1.routes import auth, balances, contracts, customers, deliveries, weighbills
 
-api_router = APIRouter()
+api_router =  APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(contracts.router, tags=["合同管理"])
 api_router.include_router(customers.router, tags=["客户管理"])
